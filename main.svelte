0ae33341154ed71b75d3599c9b4bc825f27cdc51
Rosano Coutinho <pub@xeari.com>
2020-11-22 10:20:22 -0300

    Spec OLSKModalViewCloseButton

--- main.svelte
+++ main.svelte
@@ -1,10 +1,18 @@
 <script>
+import OLSKInternational from 'OLSKInternational';
+const OLSKLocalized = function(translationConstant) {
+	return OLSKInternational.OLSKInternationalLocalizedString(translationConstant, JSON.parse(`{"OLSK_I18N_SEARCH_REPLACE":"OLSK_I18N_SEARCH_REPLACE"}`)[window.OLSKPublicConstants('OLSKSharedPageCurrentLanguage')]);
+};
+
 import OLSKStandardView from 'OLSKStandardView';
 </script>
 
 <div class="OLSKModalView">
 
 <OLSKStandardView>
+	<div slot="OLSKStandardViewToolbarHead">
+		<button class="OLSKModalViewCloseButton">{ OLSKLocalized('OLSKModalViewCloseButtonText') }</button>
+	</div>
 	<div slot="OLSKStandardViewBody">
 		<slot></slot>
 	</div>
--- ui-test-_access.js
+++ ui-test-_access.js
@@ -2,6 +2,8 @@ const kDefaultRoute = require('./controller.js').OLSKControllerRoutes().shift();
 
 Object.entries({
 	OLSKModalView: '.OLSKModalView',
+	
+	OLSKModalViewCloseButton: '.OLSKModalViewCloseButton',
 }).map(function (e) {
 	return global[e.shift()]  = e.pop();
 });
@@ -18,6 +20,10 @@ describe('OLSKModalView_Access', function () {
 
 	it('shows OLSKStandardView', function () {
 		browser.assert.elements('.OLSKStandardView', 1);
+	});
+
+	it('shows OLSKModalViewCloseButton', function () {
+		browser.assert.elements(OLSKModalViewCloseButton, 1);
 	});
 
 });
