39981bf96b19b084f5aae0dfdf5b503286b3f58b
Rosano Coutinho <pub@xeari.com>
2020-12-01 12:51:42 -0300

    Localize  OLSKModalViewCloseButtonText

--- controller.js
+++ controller.js
@@ -6,6 +6,6 @@ exports.OLSKControllerRoutes = function() {
 		OLSKRouteFunction (req, res, next) {
 			return res.OLSKLayoutRender(require('path').join(__dirname, 'stub-view'));
 		},
-		OLSKRouteLanguageCodes: ['en'],
+		OLSKRouteLanguageCodes: ['en', 'fr', 'es', 'pt'],
 	}];
 };
